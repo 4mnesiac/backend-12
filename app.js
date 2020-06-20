eb27eb633f89ca291c5c4fa549e21ad9627ddcca
Artem Glazyrin <royalbeggars@yandex.ru>
2020-06-20 17:39:46 +0500

    minor fixes

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
 /* eslint-disable no-console */
 const express = require('express');
 const path = require('path');
-const users = require('./routes/routes');
+const users = require('./routes/users');
 const cards = require('./routes/cards');
+const all = require('./routes/all');
 
 const {
   PORT = 3000,
@@ -10,12 +11,10 @@ const {
 
 const app = express();
 
-app.get('/', (req, res) => {
-  res.sendFile(`${__dirname}/public/index.html`);
-});
 app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', users);
 app.use('/', cards);
+app.use('/', all);
 
 app.listen(PORT, () => {
   console.log(`App successfully starting on port ${PORT}`);
